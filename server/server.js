1f3596ecd431167fffdc90069b034185b3d6e4ea
Lucky4014 <luckysharma4014@gmail.com>
2026-06-10 11:53:38 +0530

    Initialize Express server with MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
